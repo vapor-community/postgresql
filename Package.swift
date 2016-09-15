112d928998d35851e18403a3e7b9e30d23242b39
Nate Bird <natebird@gmail.com>
2016-09-15 11:24:13 -0400

    Add minor version number to `cpostgresql` package.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "PostgreSQL",
     dependencies: [
-   		 .Package(url: "https://github.com/vapor/cpostgresql.git", majorVersion: 0),
+   		 .Package(url: "https://github.com/vapor/cpostgresql.git", majorVersion: 0, minor: 1),
    		 .Package(url: "https://github.com/vapor/node.git", majorVersion: 0, minor: 6)
     ]
 )
