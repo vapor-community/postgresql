58dec178fb7878c064c48aa814a0c49324774f7f
Tanner Nelson <me@tanner.xyz>
2016-09-24 03:33:40 -0400

    manual pointers

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "PostgreSQL",
     dependencies: [
    		 .Package(url: "https://github.com/vapor/cpostgresql.git", majorVersion: 1),
-   		 .Package(url: "https://github.com/vapor/node.git", majorVersion: 1)
+   		 .Package(url: "https://github.com/vapor/node.git", majorVersion: 1),
+   		 .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
     ]
 )
