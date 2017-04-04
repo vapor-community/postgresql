b2c49d971dd1f533ef6533ec20ce01705ae7d16a
Nate Bird <natebird@gmail.com>
2017-04-04 16:18:14 -0400

    Update dependencies for beta release.

--- Package.swift
+++ Package.swift
@@ -3,8 +3,13 @@ import PackageDescription
 let package = Package(
     name: "PostgreSQL",
     dependencies: [
-   		 .Package(url: "https://github.com/vapor/cpostgresql.git", majorVersion: 1),
-   		 .Package(url: "https://github.com/vapor/node.git", majorVersion: 1),
-   		 .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
+        // Module map for `libpq`
+        .Package(url: "https://github.com/vapor/cpostgresql.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+
+        // Data structure for converting between multiple representations
+        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+
+        // Core extensions, type-aliases, and functions that facilitate common tasks
+        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
     ]
 )
