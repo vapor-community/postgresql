f5294eb7667dd8b1eadfe9032930131b926d9b29
Nate Bird <natebird@gmail.com>
2017-04-06 12:31:19 -0400

    Add isClosed boolean and rename connection to cConnection.

--- Sources/PostgreSQL/Database.swift
+++ Sources/PostgreSQL/Database.swift
@@ -25,6 +25,11 @@ public final class Database: ConnInfoInitializable {
     }
 
     // MARK: - Connection
+    public func makeConnection() throws -> Connection {
+        return try Connection(conninfo: conninfo)
+    }
+
+    // MARK: - Query Execution
     @discardableResult
     public func execute(_ query: String, _ values: [Node]? = [], on connection: Connection? = nil) throws -> [[String: Node]] {
         guard !query.isEmpty else {
@@ -36,6 +41,7 @@ public final class Database: ConnInfoInitializable {
         return try connection.execute(query, values)
     }
 
+    // MARK: - LISTEN
     public func listen(to channel: String, callback: @escaping (Notification) -> Void) {
         background {
             do {
@@ -44,13 +50,13 @@ public final class Database: ConnInfoInitializable {
                 try self.execute("LISTEN \(channel)", on: connection)
 
                 while true {
-                    if connection.connected != true {
-                        throw DatabaseError.cannotEstablishConnection(connection.error)
+                    if connection.isClosed == true {
+                        throw DatabaseError.cannotEstablishConnection(connection.lastError)
                     }
 
-                    PQconsumeInput(connection.connection)
+                    PQconsumeInput(connection.cConnection)
 
-                    while let pgNotify = PQnotifies(connection.connection) {
+                    while let pgNotify = PQnotifies(connection.cConnection) {
                         let notification = Notification(relname: pgNotify.pointee.relname, extra: pgNotify.pointee.extra, be_pid: pgNotify.pointee.be_pid)
 
                         callback(notification)
@@ -65,6 +71,7 @@ public final class Database: ConnInfoInitializable {
         }
     }
 
+    // MARK: - NOTIFY
     public func notify(channel: String, payload: String?, on connection: Connection? = nil) throws {
         let connection = try connection ?? makeConnection()
 
@@ -74,9 +81,5 @@ public final class Database: ConnInfoInitializable {
         else {
             try execute("NOTIFY \(channel)", on: connection)
         }
-    }
-
-    public func makeConnection() throws -> Connection {
-        return try Connection(conninfo: conninfo)
     }
 }
