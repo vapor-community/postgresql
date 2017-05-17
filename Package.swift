fb4119f263a3482d817c634a98e84e81084e8159
pruthvikar <pruthvikar@gmail.com>
2017-05-18 00:11:23 +0200

    Update Package.swift (#41)
    Update Package.swift to use Vapor 2 release dependencies

--- Package.swift
+++ Package.swift
@@ -1,17 +1,15 @@
 import PackageDescription
 
-let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
-
 let package = Package(
     name: "PostgreSQL",
     dependencies: [
         // Module map for `libpq`
-        .Package(url: "https://github.com/vapor-community/cpostgresql.git", beta),
+        .Package(url: "https://github.com/vapor-community/cpostgresql.git", majorVersion: 2),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", beta),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks
-        .Package(url: "https://github.com/vapor/core.git", beta),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2)
     ]
 )
